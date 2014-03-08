da5dfd78c4d97d11a0f17278b9069a4467e5392f
Erik Svensson <erik.public@gmail.com>
2014-03-08 15:43:20 +0100

    One exit point in klunk_size8b.

--- src/klunk_utils.c
+++ src/klunk_utils.c
@@ -6,10 +6,10 @@ uint16_t klunk_size8b(const uint16_t size)
 {
 	uint16_t s;
 	if ((size % 8) == 0) {
-		return size;
+		s = size;
 	}
 	else {
 		s = ((size / 8) + 1) * 8;
-		return s;
 	}
+	return s;
 }
